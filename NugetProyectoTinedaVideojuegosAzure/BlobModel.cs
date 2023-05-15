778652c60527b1c040597f58fc149204a2a17c3a
Raulgm111 <raul.garciamoratinos@outlook.com>
2023-05-15 21:14:05 +0200

    commit

---
+++ NugetProyectoTinedaVideojuegosAzure/BlobModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NugetProyectoTinedaVideojuegosAzure
+{
+    public class BlobModel
+    {
+        public string Nombre { get; set; }
+        public string Url { get; set; }
+        public string Contenedor { get; set; }
+    }
+}
